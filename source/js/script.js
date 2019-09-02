32f3eb6dcc5e4a14e80db8963b4a4f3c7a572e4b
DiaZzzFF <dvsh.dreamer@gmail.com>
2019-09-02 04:40:43 +0300

    Добавляет стили_3 (tablet) для header

--- source/js/script.js
+++ source/js/script.js
@@ -2,25 +2,24 @@
 
 (function () {
   var myBody = document.querySelector('body');
+  var myNav = myBody.querySelector('.nav');
+  var myBtnNavToggle = myNav.querySelector('.nav__toggle');
   var myBtnHeader = myBody.querySelector('.header__btn');
+
+  myNav.classList.remove('nav--nojs');
+
+  myBtnNavToggle.addEventListener('click', function () {
+    if (myNav.classList.contains('nav--closed')) {
+      myNav.classList.remove('nav--closed');
+      myNav.classList.add('nav--opened');
+
+    } else {
+      myNav.classList.add('nav--closed');
+      myNav.classList.remove('nav--opened');
+    }
+  });
 
   myBtnHeader.addEventListener('click', function () {
     $('.footer').animatescroll(); // временно
   });
 })();
-
-/*
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
-
-pageHeader.classList.remove('page-header--nojs');
-
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
-  }
-}); */
